09d4dcd4d33743d451600cc6bc7788f6e312bc59
CrimsonBunny <frozenknight97@gmail.com>
2021-09-28 15:24:19 +0200

    Added basic logging

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeImpl.java
@@ -1,5 +1,7 @@
 package za.ac.nwu.ac.logic.flow.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.logic.flow.CreateAccountTypeFlow;
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 @Component("createAccountTypeFlowName")
 public class CreateAccountTypeImpl implements CreateAccountTypeFlow {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CreateAccountTypeImpl.class);
+
     private final AccountTypeTranslator accountTypeTranslator;
 
     public CreateAccountTypeImpl(AccountTypeTranslator accountTypeTranslator) {
@@ -20,9 +24,19 @@ public class CreateAccountTypeImpl implements CreateAccountTypeFlow {
 
     @Override
     public AccountTypeDto create(AccountTypeDto accountType) {
+        if(LOGGER.isInfoEnabled()) {
+            String outputForLogging = "null";
+            if((null != accountType) && (null != accountType.getCreationDate())) {
+                outputForLogging = accountType.getCreationDate().toString();
+            }
+            LOGGER.info("The input object was {} and the creation date is {}", accountType, outputForLogging);
+        }
+
         if (null == accountType.getCreationDate()) {
             accountType.setCreationDate(LocalDate.now());
         }
-        return accountTypeTranslator.create(accountType);
+        AccountTypeDto result = accountTypeTranslator.create(accountType);
+        LOGGER.info("The return object is {}", result);
+        return result;
     }
 }
